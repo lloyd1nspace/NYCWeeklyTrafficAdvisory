105eaf283b2074816561f017aeaf2475313cea96
Lloyd W. Sykes <lloydwsykes@gmail.com>
2024-05-05 00:12:06 -0400

    Initial Commit

---
+++ NYCWeeklyTrafficAdvisory/NYCWeeklyTrafficAdvisoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  NYCWeeklyTrafficAdvisoryApp.swift
+//  NYCWeeklyTrafficAdvisory
+//
+//  Created by Lloyd W. Sykes on 5/5/24.
+//
+
+import SwiftUI
+
+@main
+struct NYCWeeklyTrafficAdvisoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
